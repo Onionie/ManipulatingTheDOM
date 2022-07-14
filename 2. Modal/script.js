4cfd4881e6b33182fcbc1d9eebbc6a4c667b9550
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-07-13 21:55:31 -0700

    Modal: Add/Remove Class from HTML through DOM

--- 2. Modal/script.js
+++ 2. Modal/script.js
@@ -5,6 +5,28 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Attaching eventListeners to each button
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', function () {
+    openModal();
+  });
 }
+
+btnCloseModal.addEventListener('click', function () {
+  closeModal();
+});
+
+// We can call the callback function closeModal() without the parenthesis
+// By doing so, we are "pointing" to that function and not executing it immediately
+// once the app fires up.
+overlay.addEventListener('click', closeModal);
