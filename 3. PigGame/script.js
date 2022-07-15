16b43f4b80cb792d556541bc9dc96ca69c984048
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-07-14 20:24:51 -0700

    PigGame: Starting Condition

--- 3. PigGame/script.js
+++ 3. PigGame/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting Elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const diceElement = document.querySelector('.dice');
+
+// Starting Condition
+score0Element.textContent = 0;
+score1Element.textContent = 0;
